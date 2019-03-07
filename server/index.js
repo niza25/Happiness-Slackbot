c09d78ebe6bb02665f622a520f7e682ed3c59ed7
EmineBC <eminebahce@gmail.com>
2019-03-07 11:43:29 +0100

    index edited

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const authRouter = require('./src/auth/routes');
+const userRouter = require('./src/users/routes');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app
     .use(bodyParser.json())
+    .use(authRouter)
+    .use(userRouter)
     .listen(port, () => console.log(`Listening on port ${port}`));
