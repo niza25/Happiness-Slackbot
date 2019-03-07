af53f0c8562505350ed42754137d6ce6b93618e4
Izabela <izabela.nowosada@gmail.com>
2019-03-07 10:29:07 +0100

    Add login, signup files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Login from './components/users/Login'
+import Logout from './components/users/Logaut'
+import Signup from './components/users/SignUp'
 import DashboardMain from './components/DashboardMain'
-
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <DashboardMain />
-      </div>
+      <BrowserRouter>
+      <div>
+
+        <Route exact path="/" component={Login} />
+        <Route exact path="/dashboard" component={DashboardMain} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/logout" component={Logout} />
+        <Route exact path="/signup" component={Signup} />
+        </div>
+        
+      </BrowserRouter>
     );
   }
 }
