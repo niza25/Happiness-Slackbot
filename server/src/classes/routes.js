0d15520273f0d8fe591b744b03f7bfb7d7139b85
Izabela <izabela.nowosada@gmail.com>
2019-03-11 14:40:11 +0100

    Try create routes

--- server/src/classes/routes.js
+++ server/src/classes/routes.js
@@ -1,5 +1,20 @@
 const { Router } = require("express");
 const Class = require("./model");
 const bcrypt = require("bcrypt");
+const CodClass = require('./model')
 
 const router = new Router();
+
+router.get('/classes', (req, res, next) => {
+  
+  CodClass.findAll()
+    .then(codClasses => {
+      res.json({ codClasses: codClasses })
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Something went wrong',
+        error: err
+      })
+    })
+  })
--- server/src/responses/routes.js
+++ server/src/responses/routes.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
 const Response = require("./model");
 const bcrypt = require("bcrypt");
+const Answer = require('../answers/model')
 
 const router = new Router();
+
+
