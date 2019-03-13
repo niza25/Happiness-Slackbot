710b2a15425b2048620444acf957fa1f42e2e71b
Izabela <izabela.nowosada@gmail.com>
2019-03-13 14:40:00 +0100

    Fix buttons

--- client/src/components/header/ButtonDisplay.js
+++ client/src/components/header/ButtonDisplay.js
@@ -4,11 +4,13 @@ import './header.css'
 export default function ButtonDisplay (props){
 
     return (
+      
       <div className='buttonDisplayContainer'>
-        <div className='moodButton'>
-        {props.stats}
-        <p className='smallText'>{props.topic}</p>
-        </div>
+      <div className='moodButton'>
+      {props.stats}
+      <p className='smallText'>{props.topic}</p>
       </div>
+    </div>
+      
     )
   }
--- client/src/components/header/ButtonMoodContainer.js
+++ client/src/components/header/ButtonMoodContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {getAverageAll} from '../../actions/data'
+import { getAverageAll } from '../../actions/data'
 import ButtonDisplay from './ButtonDisplay'
 import './header.css'
 
@@ -11,21 +11,19 @@ class ButtonMoodContainer extends React.Component {
   }
 
   render() {
-    this.props.averageAll && console.log(this.props.averageAll.EnergyAll[0].avg, 'ave all')
+
     return (
+
       <div className='buttonsContainer spaceAround'>
         <h5>All Classes Today:</h5>
-        <div className='buttonsContainer'>
-        {
-          this.props.averageAll &&
-          <div>
-          <ButtonDisplay topic='Energy' stats={Number(this.props.averageAll.EnergyAll[0].avg).toFixed(1)} />
-          <ButtonDisplay topic='Engagement' stats={Number(this.props.averageAll.EngagementAll[0].avg).toFixed(1)} />
-          <ButtonDisplay topic='Happinness' stats={Number(this.props.averageAll.HappinessAll[0].avg).toFixed(1)} />
+        {this.props.averageAll &&
+          <div className='buttonsContainer'>
+            <ButtonDisplay topic='Energy' stats={Number(this.props.averageAll.EnergyAll[0].avg).toFixed(1)} />
+            <ButtonDisplay topic='Engagement' stats={Number(this.props.averageAll.EngagementAll[0].avg).toFixed(1)} />
+            <ButtonDisplay topic='Happinness' stats={Number(this.props.averageAll.HappinessAll[0].avg).toFixed(1)} />
           </div>
         }
-          
-        </div>
+
       </div>
     )
   }
@@ -35,4 +33,5 @@ const mapStateToProps = state => ({
   averageAll: state.averageAll
 })
 
-export default connect(mapStateToProps, {getAverageAll})(ButtonMoodContainer)
+export default connect(mapStateToProps, { getAverageAll })(ButtonMoodContainer)
+
